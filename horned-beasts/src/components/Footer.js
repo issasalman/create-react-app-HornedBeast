cef84a3a176994dd5ff802ad73e5560a26dcec86
issasalman <issasalmanhu1996@gmail.com>
2021-08-29 15:39:15 +0300

    done lab01

---
+++ horned-beasts/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+     <footer>
+    
+     <h1>
+     Issa Salman 2021 ASAC
+     </h1>
+    
+     </footer>
+     
+    )
+  }
+}
+
+export default Footer;
