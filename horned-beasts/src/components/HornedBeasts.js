eaf31aff082f01dd359a3a7a1f7f77501e0ee6fd
issasalman <issasalmanhu1996@gmail.com>
2021-08-30 15:28:25 +0300

    done lab 02

--- horned-beasts/src/components/HornedBeasts.js
+++ horned-beasts/src/components/HornedBeasts.js
@@ -1,18 +1,63 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
+
 
 class HornedBeasts extends React.Component {
 
+  
+
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      numOfHornedVotes: 0
+       
+
+    };
+  }
+  increaseHorneVotes = () => {
+    this.setState({
+      numOfHornedVotes: this.state.numOfHornedVotes + 1 
+      
+    });
+    
+
+  }
  
+
   render() {
-    console.log('props: ', this.props);
+
+
     return (
       <div>
-        <h2>{this.props.title}</h2>
-        <p>{this.props.description}</p>
-        <img src={this.props.imageUrl} alt="" />
+       
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={this.props.image_url } onClick={this.increaseHorneVotes}  />
+          <Card.Body>
+            <Card.Title>The Title is {this.props.title}</Card.Title>
+            <Card.Text>
+              number of horns {this.props.horns}
+            </Card.Text>
+            <Card.Text>
+             Summary:- {this.props.desc}
+            </Card.Text>
+            <Card.Text>
+              Number of votes : {this.state.numOfHornedVotes} 💓
+              {/*               
+    function issa() {
+      if (this.state.numOfHornedVotes === 1) { 
+                    
+        hi}
+     }; */}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
 }
 
+
 export default HornedBeasts;
+
