af1b8396e6bfdbd1d001a584701029ccbf9b3ebf
issasalman <issasalmanhu1996@gmail.com>
2021-08-31 22:48:27 +0300

    done lab03

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import React from 'react'; 
-
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SelectedBeast from './components/SelectedBeast.js';
+import Horned from './assets/data.json';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      AllHorned:Horned,
+      show: false,
+      theSelectedOne: {},
+    };
+  }
+  
 
-  render() { 
+  showModal = (title) => {
+    
+    let selectedArr =Horned.find(element => {
+      return(element.title === title);
+      
+      
+    });
+    
+    this.setState({
+      show: true,
+      theSelectedOne: selectedArr,
+    });
+    return selectedArr ;
+    
+  };
+
+  handleClose = () => {
+    this.setState({ show: !this.state.show
+     
+    
+    });
+  }
+  
+  render() {
     return (
       <div>
-      
         <Header />
-        <Main />
+        <Main
+          theSelectedOne={this.state.theSelectedOne}
+          showModal={this.showModal}
+        />
+        <SelectedBeast
+            
+          handleClose={this.handleClose}
+          show={this.state.show}
+          theSelectedOne={this.state.theSelectedOne} 
+        />
         <Footer />
-        
       </div>
     );
   }
 }
 
-export default App; 
+export default App;
